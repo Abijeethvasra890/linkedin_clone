b3b65346f97ca40f4e4cf2fff022d18a5297d685
Abijeeth <vasraabijeeth@gmail.com>
2024-02-11 14:20:14 +0530

    feedInput

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import SideBar from './SideBar';
+import Feed from './Feed';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       {/*App Body*/}
       <div className='app__body'>
         <SideBar />
-        
+        <Feed />
       </div>
     </div>
   );
